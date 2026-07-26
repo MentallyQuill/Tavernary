945a89bde98d37df58b02abb0d86f555367c4cec
Josh Shapiro <kep359@gmail.com>
2026-07-26 08:31:10 -0600

    Prettier fix to About page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -80,8 +80,8 @@ export default function AboutPage() {
             documentation, licenses, and support.
           </p>
           <p>
-            If you've made something for the community, or know of a killer 
-            extension, prompt, or frontend that isn't on Tavernary, you can add 
+            If you hvae made something for the community, or know of a killer 
+            extension, prompt, or frontend that is not on Tavernary, you can add 
             it to the catalog and help make the next discovery easier for someone 
             else.
           </p>
